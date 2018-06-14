8fd0bf1c26462e7fce36332779822e2a3c454039
pejman hkh <pejman-hkh@users.noreply.github.com>
2018-06-14 17:43:26 +0430

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "mysqlWrapper.h"
+#include <signal.h>
+#include "utils.h"
+
+mysqlWrapper db;
+
+int main() {
+
+	db.connect_uc("localhost", "root", "123456" ).switchDb("db_name");
+
+
+	mysqlResult res = db.prepare("SELECT * FROM test_db LIMIT 0,1").execute();
+	res.next();
+
+
+	std::cout << res.getString("title");
+
+
+	//update query
+	db.prepare("UPDATE test_db SET title = ? WHERE id = ? ")\
+	.setString("test it")\
+	.setInt(1)\
+	.execute();
+
+
+
+	//insert query
+	db.prepare("INSERT INTO test_db(title) VALUES(?)")\
+	.setString("ttttt")\
+	.execute();
+
+
+	//
+	mysqlResult res1 = db.prepare("SELECT title FROM test_db LIMIT 0,10").execute();
+	
+	int i = 0;
+	while( res1.next() ) {
+
+		std::cout << "res1 result " << res1.getString("title") << std::endl;
+	
+
+		//test nested query		
+		mysqlResult res2 = db.prepare("SELECT title FROM test_db LIMIT ?,1").setInt(i++).execute();
+		res2.next();
+
+		std::cout << "res2 result" << res2.getString("title") << std::endl;
+
+	}
+
+
+	return 0;
+}
